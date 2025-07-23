763a409c7d887d490f619bb5c54cc2c72fd1e014
Pedro Germano <pedrogermano232@gmail.com>
2025-07-23 16:20:52 -0300

    feat: variant config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import Text from "./components/text";
+
 export default function App() {
-  return <div className="text-green-base">Olá Mundo!</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base">
+        Olá Mundo!
+      </Text>
+      <Text className="text-green-base">Olá Mundo!</Text>
+      <Text variant="body-md-bold">Olá Mundo!</Text>
+    </div>
+  );
 }
